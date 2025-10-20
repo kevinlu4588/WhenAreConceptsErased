3ee02592754a78c3f8481f2cabae90ec0b58a73c
Kevin Lu <kevinlu4588@gmail.com>
2025-10-20 11:05:43 -0400

    Test commit

--- new_probe_code/config.py
+++ new_probe_code/config.py
@@ -5,4 +5,4 @@ class ProbeConfig:
         self.variance_scales = [1.00, 1.02, 1.03, 1.04]
         self.score_type = "CLIP"  # or "classification"
         self.base_images_path = "/share/u/kevin/DiffusionConceptErasure/new_probe_code/results/base_model/"
-        self.mask_size = (256, 256)  # width, height in pixels
+        self.mask_size = (256, 256)  # width, height in pixels 
